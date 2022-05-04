d3716d4925824b03e4626a1fcafedd8989e985cb
Iraklis Roussos <iraklisroussos@gmail.com>
2022-05-04 12:20:35 +1000

    First if statement

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-let firstCard = 3
-let secondCard = 8
+// Check if the person is old enough to enter the nightclub (18)
+// Log suitable message to the console in both cases
 
-let sum = firstCard + secondCard
+let age = 17
+
+if (age >= 18){
+    console.log("Welcome!")
+} else{
+    console.log("You cannot enter the club!")
+}
