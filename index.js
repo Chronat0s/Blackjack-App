6aba6452795831a9c783acbe7dd338cbfdc4fc93
Iraklis Roussos <iraklisroussos@gmail.com>
2022-05-04 13:01:54 +1000

    display message on game interface

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 let firstCard = 10
-let secondCard = 11
+let secondCard = 12
 let sum = firstCard + secondCard
 let hasBlackJack = false
+let isAlive = true
+let message = ""
+let messageEl = document.getElementById("message-el")
 
-if (sum <= 20){
-    console.log("Do you want to draw a new card? 🙂")
-} else if (sum === 21){
-    console.log("You've got Blackjack! 🥳")
-    hasBlackJack = true
-} else {
-    console.log("You're out of the game! 😭")
+function startGame(){
+    if (sum <= 20){
+        message = "Do you want to draw a new card? 🙂"
+    } else if (sum === 21){
+        message = "You've got Blackjack! 🥳"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game! 😭"
+        isAlive = false
+    }
+    
+    // CASH OUT!
+    messageEl.textContent = message
 }
-
-// CASH OUT!
-console.log(hasBlackJack)
