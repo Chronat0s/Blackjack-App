212ab8c1803591cacfe12601ad842000d2c9d64a
Iraklis Roussos <iraklisroussos@gmail.com>
2022-05-07 17:52:28 +1000

    Objects & Functions Challenge 1

--- objects.js
+++ objects.js
@@ -1,13 +1,11 @@
-// Create an object that represents an airbnb castle listing.
-// It should contain at least one boolean, one string, one number and one array
-// Log out at least two of the keys using the dot notation
-
-let airbnb = {
-    title: "Live like a king",
-    price: 160,
-    features: ["4 guests", "1 bathroom", "2 beds"],
-    isSuperHost: true
+person = {
+    name: "Iraklis",
+    age: 21,
+    country: "Australia"
 }
 
-console.log(airbnb.price)
-console.log(airbnb.features)
+function logData(){
+    console.log(person.name + " is " + person.age + " years old and lives in " + person.country)
+}
+
+logData()
