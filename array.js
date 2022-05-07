55796c296f448121fc11f049f8dba6b6a3c2dfca
Iraklis Roussos <iraklisroussos@gmail.com>
2022-05-07 16:26:04 +1000

    new cards are displayed on the cards section

--- array.js
+++
@@ -1,6 +0,0 @@
-let sentence = ["Hello", "my", "name", "is", "Iraklis"]
-let greetingEl = document.getElementById("greeting-el")
-
-for (let i = 0; i < sentence.length; i++){
-    greetingEl.textContent += sentence[i] + " "
-}
