003adae963f23ecf306ee11ba3f4052d83d004ba
Iraklis Roussos <iraklisroussos@gmail.com>
2022-05-07 17:07:45 +1000

    learnt OR function

--- logicalAndOperator.js
+++ logicalAndOperator.js
@@ -1,10 +1,9 @@
-let hasSolvedChallenge = true
-let hasHintsLeft = false
-
-if (hasSolvedChallenge === false && hasHintsLeft === false){
-    showSolution()
+likesDocumentaries = false
+likesStartups = false
+if (likesDocumentaries === true || likesStartups === true){
+    recommendMovie()
 }
 
-function showSolution(){
-    console.log("Showing the solution....")
+function recommendMovie(){
+    console.log("Hey,check out this new film we think you will like!")
 }
