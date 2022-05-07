90d7d1c4bae40e02e44780d9338f8616722d0613
Iraklis Roussos <iraklisroussos@gmail.com>
2022-05-07 17:05:32 +1000

    learnt the Logical AND operator

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 //VARIABLES
-
-let firstCard = getRandomCard()
-let secondCard = getRandomCard()
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard 
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.querySelector("#sum-el")
@@ -18,6 +15,11 @@ function getRandomCard(){
 }
 
 function startGame(){
+    let isAlive = true
+    let firstCard =  getRandomCard()
+    let secondCard =  getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
     renderGame()
 }
 
