cc69d015e17a1d40e889061485ebcf5e0918a944
Iraklis Roussos <iraklisroussos@gmail.com>
2022-05-07 16:21:02 +1000

    For loops, arrays and DOM

--- array.js
+++ array.js
@@ -1,3 +1,6 @@
-for (let count = 10; count < 21; count += 1) {
-    console.log(count)
+let sentence = ["Hello", "my", "name", "is", "Iraklis"]
+let greetingEl = document.getElementById("greeting-el")
+
+for (let i = 0; i < sentence.length; i++){
+    greetingEl.textContent += sentence[i] + " "
 }
